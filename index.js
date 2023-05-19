a9ab578260e62e9a9e7a3067cb3d39f03edab161
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-05-19 12:02:27 -0500

    Implementando la plantilla NODE del laboratorio

--- index.js
+++ index.js
@@ -2,5 +2,13 @@
 //npm install body-parser
 //npm install markdown-it --save
 //npm install express
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const app = express()
+app.use(express.static('pub'))
 
-const app = require('express');     
+app.listen(3000, () => {
+    console.log("Escuchando en http://localhost:3000")
+});
+     
