e7f865c97c6b7afcdd7be04d19e5a42f0877b581
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-05-24 01:49:31 -0500

    Creando el formulario para ingresar markdown

--- pub/client.js
+++ pub/client.js
@@ -1,6 +1,9 @@
 //Lado del cliente
 //Creando la funcion
 function crearNuevoArchivo(){
-    let remplazo = `<h2>Esto es un nuevo Archivo</h2>`;
+    let remplazo = `<h4>Titulo</h4>
+                    <input type="text" id="textTitle" size="50"><br><br>
+                    <h4>Ingrese Contenido Markdown</h4>
+                    <textarea type="verTexto" rows="30" cols="80"></textarea>`;
     document.getElementById("s2").innerHTML = remplazo;
 }
