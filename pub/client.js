d5682387ffdbd32c01de7f759e04b8e7f2eb2205
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-05-24 01:51:59 -0500

    Agregando el boton para almacenar el archivo nuevo

--- pub/client.js
+++ pub/client.js
@@ -4,6 +4,7 @@ function crearNuevoArchivo(){
     let remplazo = `<h4>Titulo</h4>
                     <input type="text" id="textTitle" size="50"><br><br>
                     <h4>Ingrese Contenido Markdown</h4>
-                    <textarea type="verTexto" rows="30" cols="80"></textarea>`;
+                    <textarea type="verTexto" rows="30" cols="80"></textarea><br>
+                    <input type="button" id="almacenarArchivo" value="Creando Archivo" onclick="almacenarArchivoNuevo()">`;
     document.getElementById("s2").innerHTML = remplazo;
 }
